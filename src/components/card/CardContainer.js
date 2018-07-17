9cfd97cd0bc4e59a91badb86bbced4ea74b506a1
Wade Stewart <stew.wade@gmail.com>
2018-07-17 17:51:09 -0400

    fetching artist data

--- src/components/card/CardContainer.js
+++ src/components/card/CardContainer.js
@@ -10,14 +10,34 @@ class CardContainer extends Component {
         timer:          10,
     }
 
-    componentDidMount = (token) => {
+    componentDidMount = () => {
         const request           = require('superagent')
-        const traverson         = require('traverson')
-        const JsonHalAdapter    = require('traverson-hal')
-        let xappToken = token
 
-        traverson.registerMediaType(JsonHalAdapter.mediaType, JsonHalAdapter)
-        let api = 
+        this.requestWithToken = (token) => {
+            const traverson         = require('traverson')
+            const JsonHalAdapter    = require('traverson-hal')
+            let xappToken = token
+    
+            traverson.registerMediaType(JsonHalAdapter.mediaType, JsonHalAdapter)
+            let api = traverson.from('https://api.artsy.net/api').jsonHal()
+    
+            api.newRequest()
+                .follow('artist')
+                .withRequestOptions({
+                    headers: {
+                        'X-Xapp-Token': xappToken,
+                        'Accept': 'application/vnd.artsy-v2+json'
+                    }
+                })
+                .withTemplateParameters({ id: 'andy-warhol' })
+                .getResource((err, data) => {
+                    if (data) {
+                        console.log(data)
+                    } else {
+                        console.log(err)
+                    }
+                })
+        }
 
         request
             .post(config.apiUrl)
@@ -27,7 +47,7 @@ class CardContainer extends Component {
             })
             .then((res) => {
                 config.xappToken = res.body.token
-                this.componentDidMount(config.xappToken)
+                this.requestWithToken(config.xappToken)
             })
 
     }
