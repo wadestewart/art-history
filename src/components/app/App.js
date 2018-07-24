347a756d4c34c10e98527d3c2bff06202ceff405
Wade Stewart <stew.wade@gmail.com>
2018-07-24 15:10:47 -0400

    beginning of promise chain working!

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -6,20 +6,44 @@ import ArtDetail from '../art-detail/ArtDetail'
 
 class App extends Component {
   
-  // I can create an array with an empty object and chain promises in the fetch response to fetch image data - use concat (not push) in React.
+  // I can create an empty array for images and chain promises in the fetch response to fetch image data - use concat (not push) in React.
   state = {
     artworks: [],
+    images: [],
     current:  {}
   }
 
   componentDidMount = () => {
+     
+    const a = fetch(`${API.apiUrl}?method=cooperhewitt.objects.getOnDisplay&access_token=${API.apiKey}&per_page=5`)
+        a.then(res => {
+          res.json()
+            .then(data => {
+              console.log(data)
+              this.setState({
+                    artworks: data.objects
+                  })
+            })
+            // a.then(data => this.setState({ artworks: data.objects }))
+        })
+ 
+        a.catch(err => console.log(err))
+    
+    // console.log(this.state.artworks)
 
-    fetch(`${API.apiUrl}?method=cooperhewitt.objects.getOnDisplay&access_token=${API.apiKey}`)
-        .then(res => res.json())
-        .then(data => this.setState({ artworks: data.objects }))
-        .catch(err => console.log(err))
+    // const images = artworks.map((artwork, key) => {
+    //   return (
+    //     artwork
+    //   )
+    // })
+
+    // fetch(`${API.apiUrl}?method=cooperhewitt.objects.getImages&access_token=${API.apiKey}&id=${this.state.artworks.id}`)
+    // const images = art.map((image, key) => {
+      //   this.setState({ images: image})
+      // })
+      // .then(art => console.log(art))
   }
-
+      
   render() {
     let artworks = this.state.artworks
     // console.log(artworks)
