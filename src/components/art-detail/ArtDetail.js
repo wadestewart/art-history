e59f8b8eb517540e4602ffbdccd780cb2db61e0f
Wade Stewart <stew.wade@gmail.com>
2018-07-27 15:15:09 -0400

    passing all data to ArtDetail component

--- src/components/art-detail/ArtDetail.js
+++ src/components/art-detail/ArtDetail.js
@@ -1,26 +1,28 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class ArtDetail extends Component {
-    render() {
-        console.log(this.props.images)
-        let bigPics = this.props.images.b
-        let bigPicData = []
-        
-        for (let key in bigPics) {
-            // console.log(key, bigPics[key])
-            bigPicData.push(bigPics[key])
-        }
-        let largeImgUrl = bigPicData[0]
-
-
-        return (
-            <div className="art-details">
-                <figure>
-                    <img src={largeImgUrl} alt="" />
-                </figure>
-            </div>
-        )
+function ArtDetail(props) {
+    console.log(props.images)
+    let bigPics = props.images.b
+    let bigPicData = []
+    
+    for (let key in bigPics) {
+        // console.log(key, bigPics[key])
+        bigPicData.push(bigPics[key])
     }
+    let largeImgUrl = bigPicData[0]
+
+    return (
+        <div className="art-details">
+            <figure>
+                <img src={largeImgUrl} alt="" />
+                <h1 className="piece-title">{props.artwork.title}</h1>
+            </figure>
+            <div className="detailed-info">
+                <h2>{props.artwork.creditline}</h2>
+                <p>{props.artwork.description}</p>
+            </div>
+        </div>
+    )
 }
 
 export default ArtDetail
