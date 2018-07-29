f8ebfa69bedfaade36ca092d6af2df1d30dd5935
Wade Stewart <stew.wade@gmail.com>
2018-07-29 10:01:43 -0400

    removed some padding from ArtDetail

--- src/components/flashcard/Flashcard.js
+++ src/components/flashcard/Flashcard.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
 
 class Flashcard extends Component {
+
+    state = {
+
+    }
+    
     render() {
         return (
             <div>
-                
+
             </div>
         )
     }
