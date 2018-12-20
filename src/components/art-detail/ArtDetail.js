991f79d81fd37159d5b954bf6a2ea83d627a5680
Wade Stewart <stew.wade@gmail.com>
2018-12-20 12:19:33 -0500

    conditional rendering added to art-detail based on data availability

--- src/components/art-detail/ArtDetail.js
+++ src/components/art-detail/ArtDetail.js
@@ -26,7 +26,37 @@ function ArtDetail(props) {
                 </div>
             </div>
         )
-    } else if (props.artwork.label_text !== null) {
+    } else if (props.artwork.title !== null && props.artwork.title !== "" && props.artwork.gallery_text !== null) {
+        detail = (
+            <div className="art-detail">
+                <figure className="large-image">
+                    <img src={largeImgUrl} alt="" />
+                </figure>
+                <div className="detailed-info">
+                    <h3 className="title-h3">Artwork Title:</h3>
+                    <h4 className="piece-title">{props.artwork.title}</h4>
+                    <h3 className="details-h3">Artwork Details:</h3>
+                    <h4 className="label-text">{props.artwork.gallery_text}</h4>
+                    <h4 className="art-credit">{props.artwork.creditline}</h4>
+                </div>
+            </div>
+        )
+    } else if (props.artwork.title_raw !== null && props.artwork.title_raw !== "" && props.artwork.label_text !== null) {
+        detail = (
+            <div className="art-detail">
+                <figure className="large-image">
+                    <img src={largeImgUrl} alt="" />
+                </figure>
+                <div className="detailed-info">
+                    <h3 className="title-h3">Artwork Title:</h3>
+                    <h4 className="piece-title">{props.artwork.title_raw}</h4>
+                    <h3 className="details-h3">Artwork Details:</h3>
+                    <h4 className="label-text">{props.artwork.label_text}</h4>
+                    <h4 className="art-credit">{props.artwork.creditline}</h4>
+                </div>
+            </div>
+        )
+    } else if (props.artwork.title !== null && props.artwork.title !== "" && props.artwork.label_text !== null) {
         detail = (
             <div className="art-detail">
                 <figure className="large-image">
