6c2ce26503d8a52b1a06f7f511e23a4495fb3278
Wade Stewart <stew.wade@gmail.com>
2019-01-21 23:27:14 -0500

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app) and uses the [Cooper Hewitt API](https://collection.cooperhewitt.org/api/).
-
---
+++ precache-manifest.5699eb9081acb0d2de912c591f648fd7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "16e3454f660db759154e",
+    "url": "/art-history/static/css/main.feb108d6.chunk.css"
+  },
+  {
+    "revision": "16e3454f660db759154e",
+    "url": "/art-history/static/js/main.16e3454f.chunk.js"
+  },
+  {
+    "revision": "696988e92183e13ca1c3",
+    "url": "/art-history/static/js/1.696988e9.chunk.js"
+  },
+  {
+    "revision": "531fc8a77d029fdb2e4e",
+    "url": "/art-history/static/js/runtime~main.531fc8a7.js"
+  },
+  {
+    "revision": "ab5a724451e3d171718cb56918c13f0a",
+    "url": "/art-history/static/media/cooperhewitt.ab5a7244.jpg"
+  },
+  {
+    "revision": "a5e54861933d0eeaf26234c1830557e7",
+    "url": "/art-history/index.html"
+  }
+];
